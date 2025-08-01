6f7b7b3e79d80688dace8c260084def6534f4d74
aqib gul <aqibgul880@gmail.com>
2025-08-01 16:24:06 +0500

    random joke generator

--- app/page.js
+++ app/page.js
@@ -1,103 +1,88 @@
+"use client";
 import Image from "next/image";
+import image from "@/public/joke-pic.jpg"; // Ensure this path is correct
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [joke, setJoke] = useState({});
+  const [showJoke, setShowJoke] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  const apiUrl = "https://official-joke-api.appspot.com/random_joke";
+  const fetchJoke = async () => {
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const jokeData = await response.json();
+      console.log(jokeData);
+
+      setJoke(jokeData);
+    } catch (error) {
+      console.error("Error fetching joke:", error);
+      alert("Failed to fetch a joke. Please try again later.");
+    }
+  };
+  useEffect(() => {
+    fetchJoke();
+  }, []);
+
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <div className="flex-col bg-blue-50 border-2 md:w-[50%] md:h-[50%] w-full p-3  ">
+          <h1 className="text-2xl font-bold text-center my-8  ">
+            Random Jokes Generator
+          </h1>
+          <div className="flex justify-center mb-8">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className="rounded-full shadow-lg"
+              src={image}
+              alt="Joke Icon"
+              width={300}
+              height={300}
+              placeholder="blur"
+              blurDataURL=""
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className="text-center mb-4">
+            <h2 className="text-2xl font-semibold mb-2">{joke.setup} </h2>
+            {showJoke ? (
+              <div>
+                <p className="text-2xl">{joke.punchline}</p>
+                <button
+                  onClick={() => setShowJoke(false)}
+                  className="bg-foreground text-white px-4 py-2 rounded hover:shadow-lg"
+                >
+                  Hide Answer
+                </button>
+              </div>
+            ) : (
+              <button
+                onClick={() => setShowJoke(true)}
+                className="bg-foreground text-white px-4 py-2 rounded hover:shadow-lg"
+              >
+                Show Answer
+              </button>
+            )}
+          </div>
+
+          <div className="flex justify-center mt-4">
+            <button
+              onClick={() => {
+                setShowJoke(false);
+                fetchJoke();
+              }}
+              className="bg-foreground text-white px-4 py-2 rounded hover:shadow-lg "
+            >
+              Next
+            </button>
+          </div>
+          <p className="text-lg text-center">
+            Click the button to get a random joke!
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
